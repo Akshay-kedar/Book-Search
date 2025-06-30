09be24c806e075d68675ae6ae8a8a0eae4053893
Akshay Kedar <akshay.kedar@das.cpa.com>
2025-06-30 13:26:23 +0530

    Adding Paginator component

---
+++ src/app/paginated-list/paginated-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-paginated-list',
+  templateUrl: './paginated-list.component.html',
+  styleUrls: ['./paginated-list.component.css']
+})
+export class PaginatedListComponent {
+
+  @Input()
+  items:string[]=[];
+
+  @Input()
+  pageSize:number=5;
+
+  currentPage: number = 0;
+
+   get paginatedItems(): string[] {
+
+    const startIndex= this.currentPage*this.pageSize;
+
+
+    return this.items.slice(startIndex,startIndex+this.pageSize)
+   }
+
+ onPageChange(event: any): void {
+    this.currentPage = event.page;
+  }
+
+}
